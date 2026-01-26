b6bd83df15ef4ec83317ecfc64cb454fe96fab4a
vanessa-godoi <nessagodoiii@gmail.com>
2026-01-26 18:23:15 -0300

    add: create permission set and attach inline policy v4

--- locals.tf
+++ locals.tf
@@ -4,7 +4,6 @@ locals {
     for f in local.permission_files :
     yamldecode(file("${path.module}/permissions.d/${f}"))
   ]
-  instance_sso        = data.aws_ssoadmin_instances.sso.arns[0]
-  permission_set_arns = data.aws_ssoadmin_permission_sets.arns.arns[0]
+  instance_sso = data.aws_ssoadmin_instances.sso.arns[0]
 }
 
